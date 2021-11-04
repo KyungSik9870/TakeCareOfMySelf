928bb18098c37f02edd460129090f623bfb1fd78
KyungSik9870 <rudtlr9870@gmail.com>
2021-11-04 21:56:45 +0900

    initial commit

---
+++ src/main/java/com/github/takecareofmyself/TakeCareOfMySelfApplication.java
@@ -0,0 +1,13 @@
+package com.github.takecareofmyself;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TakeCareOfMySelfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TakeCareOfMySelfApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/takecareofmyself/TakeCareOfMySelfApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.takecareofmyself;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TakeCareOfMySelfApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
