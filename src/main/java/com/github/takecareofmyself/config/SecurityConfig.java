21fac4c23801d6caf9861ea8c71e4d9acd04194c
KyungSik9870 <rudtlr9870@gmail.com>
2021-11-30 20:19:19 +0900

    feat: Spring Security 설정 클래스 추가

---
+++ src/main/java/com/github/takecareofmyself/config/SecurityConfig.java
@@ -0,0 +1,18 @@
+package com.github.takecareofmyself.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.csrf().disable().authorizeRequests()
+			.mvcMatchers("/", "/sign-up", "/login").permitAll()
+			.anyRequest().authenticated();
+	}
+}
